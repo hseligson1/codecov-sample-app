3fbfd673532ac731326c060e066ae96064aab53c
hseligson1 <hannah.seligson@codefresh.io>
2021-08-11 16:58:17 -0400

    testing tom's test

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,26 @@ const printName = (firstName: string, lastName: string) => {
     const fullName = firstName + " " + lastName;
     console.log(fullName);
     return fullName;
-  }
-module.exports = printName;
+}
+
+const add = (firstNumber: number, secondNumber: number) => {
+    return firstNumber + secondNumber
+}
+
+const subtract = (firstNumber: number, secondNumber: number) => {
+    return firstNumber - secondNumber
+}
+
+const divide = (firstNumber: number, secondNumber: number) => {
+    if (secondNumber === 0) {
+      return 0
+    }
+    return firstNumber/secondNumber;
+}
+
+module.exports = {
+  add,
+  divide,
+  printName,
+  subtract,
+}
