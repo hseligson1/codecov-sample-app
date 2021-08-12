b44243249d1379b79ac1ca7dd02c031b2aa62666
hseligson1 <hannah.seligson@codefresh.io>
2021-08-12 07:26:12 -0400

    Revert "testing tom's test"
    This reverts commit 3fbfd673532ac731326c060e066ae96064aab53c.

--- build/index.js
+++ build/index.js
@@ -4,22 +4,5 @@ var printName = function (firstName, lastName) {
     console.log(fullName);
     return fullName;
 };
-var add = function (firstNumber, secondNumber) {
-    return firstNumber + secondNumber;
-};
-var subtract = function (firstNumber, secondNumber) {
-    return firstNumber - secondNumber;
-};
-var divide = function (firstNumber, secondNumber) {
-    if (secondNumber === 0) {
-        return 0;
-    }
-    return firstNumber / secondNumber;
-};
-module.exports = {
-    add: add,
-    divide: divide,
-    printName: printName,
-    subtract: subtract,
-};
+module.exports = printName;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -2,26 +2,5 @@ const printName = (firstName: string, lastName: string) => {
     const fullName = firstName + " " + lastName;
     console.log(fullName);
     return fullName;
-}
-
-const add = (firstNumber: number, secondNumber: number) => {
-    return firstNumber + secondNumber
-}
-
-const subtract = (firstNumber: number, secondNumber: number) => {
-    return firstNumber - secondNumber
-}
-
-const divide = (firstNumber: number, secondNumber: number) => {
-    if (secondNumber === 0) {
-      return 0
-    }
-    return firstNumber/secondNumber;
-}
-
-module.exports = {
-  add,
-  divide,
-  printName,
-  subtract,
-}
+  }
+module.exports = printName;
